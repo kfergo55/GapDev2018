a1c542a1698b00e5b312b5a09543da77102daa3c
kfergo55 <kfergo55@gmail.com>
2019-03-28 16:48:04 +1100

    add images

--- Data-ModelR/app.R
+++ Data-ModelR/app.R
@@ -26,64 +26,31 @@ library(readxl)
 excel_path1 <- "~/Adventure_Works_Data_Definitions.xlsx"
 excel_path2 <- "~/Adventure_Works_version_2.xlsx"
 
-# store sheet names for tables
-table_names <- c("Employee (HR)","Employee Dept History",  
-                 "Business Entity Address", "Sales Person",           
-                 "Contact", "Employee Pay History",   
-                 "Address", "Sales Territory",
-                 "Sales Order Header")
-
 # read in the sheets - 
-EmployeeHR <- read_excel(path=excel_path1, sheet = 1)
-EmployeeDeptHistory <- read_excel(path=excel_path1, sheet = 2)
-BusinessEntityAddress <- read_excel(path=excel_path1, sheet = 3)
-Salesperson <- read_excel(path=excel_path1, sheet = 4)
-Contact <- read_excel(path=excel_path1, sheet = 5)
-EmployeePayHistory <- read_excel(path=excel_path1, sheet = 6)
-Address <- read_excel(path=excel_path1, sheet = 7)
+EmployeeHR <- read_excel(path=excel_path2, sheet = 1)
+BusinessEntityAddress <- read_excel(path=excel_path2, sheet = 2)
+Salesperson <- read_excel(path=excel_path2, sheet = 3)
+Contact <- read_excel(path=excel_path2, sheet = 4)
+EmployeePayHistory <- read_excel(path=excel_path2, sheet = 5)
+Address <- read_excel(path=excel_path2, sheet = 6)
 SalesTerritory <- read_excel(path=excel_path1, sheet = 8)
 SalesOrderHeader <- read_excel(path=excel_path1, sheet = 9)
 
-# reshape each "table" into the format that datamodelr requires 
-# first column contents becomes the column names 
-# except for SalesTerritory and SalesOrderHeader as they are in the correct format
-EmployeeHR <- as.data.frame(t(EmployeeHR$column))
-names(EmployeeHR) <- as.character(unlist(EmployeeHR[1,]))
-
-EmployeeDeptHistory <- as.data.frame(t(EmployeeDeptHistory$column))
-names(EmployeeDeptHistory) <- as.character(unlist(EmployeeDeptHistory[1,]))
-
-BusinessEntityAddress <- as.data.frame(t(BusinessEntityAddress$column))
-names(BusinessEntityAddress) <- as.character(unlist(BusinessEntityAddress[1,]))
-
-Salesperson <- as.data.frame(t(Salesperson$column))
-names(Salesperson) <- as.character(unlist(Salesperson[1,]))
-
-Contact <- as.data.frame(t(Contact$column))
-names(Contact) <- as.character(unlist(Contact[1,]))
-
-EmployeePayHistory <- as.data.frame(t(EmployeePayHistory$column))
-names(EmployeePayHistory) <- as.character(unlist(EmployeePayHistory[1,]))
-
-Address <- as.data.frame(t(Address$column))
-names(Address) <- as.character(unlist(Address[1,]))
-
 # use datamodelr:dm_from_data_frames to create the data model
-dm <- dm_from_data_frames(EmployeeHR, EmployeeDeptHistory, BusinessEntityAddress,
+datamodel <- dm_from_data_frames(EmployeeHR, BusinessEntityAddress,
                                  Salesperson, Contact, EmployeePayHistory, Address, 
                                  SalesTerritory, SalesOrderHeader)
 
 # add the keys to the data model
-dm <- dm_add_references(
+datamodel <- dm_add_references(
   datamodel,
-  EmployeeDeptHistory$`Business Entity ID` == EmployeeHR$`Business Entity ID`,
   BusinessEntityAddress$`Business Entity ID` == EmployeeHR$`Business Entity ID`,
-  BusinessEntityAddress$AddressID == Address$AddressID,
-  Salesperson$SalesPersonID == EmployeeHR$`Business Entity ID`,
-  Contact$`Business Entity ID` == EmployeeHR$`Business Entity ID`,
-  EmployeePayHistory$EmployeeID == EmployeeHR$`Business Entity ID`,
-  Address$TerritoryID == SalesTerritory$`Territory ID`,
-  SalesOrderHeader$`Sales Person ID` == Salesperson$SalesPersonID,
+  BusinessEntityAddress$`Address ID` ==Address$`Address ID`,
+  Salesperson$`Business Entity ID` == EmployeeHR$`Business Entity ID`,
+  Contact$`BusinessEntityID (Person)` == EmployeeHR$`Business Entity ID`,
+  EmployeePayHistory$`Business Entity ID` == EmployeeHR$`Business Entity ID`,
+  Address$`Territory ID` == SalesTerritory$`Territory ID`,
+  SalesOrderHeader$`Sales Person ID` == Salesperson$`Business Entity ID`,
   SalesOrderHeader$`Territory ID` == SalesTerritory$`Territory ID`)
 
 # SHINY ADAPTION - move the following block to the shiny server section
@@ -107,7 +74,7 @@ get_graph <- function(x, rankdir = "RL", view_type = "all") {
   
   ret <- ""
   try({
-    graph <- dm_create_graph(dm, graph_attrs = "bgcolor = '#F4F0EF', splines = ortho, model = subset",
+    graph <- dm_create_graph(datamodel, graph_attrs = "bgcolor = '#F4F0EF', splines = ortho, model = subset",
                              rankdir = rankdir, view_type = view_type, 
                              edge_attrs = "dir = both, arrowtail = crow, arrowhead = odiamond",
                              node_attrs = "fontname = 'Arial'")
@@ -157,14 +124,14 @@ server = function(input, output, session){
   
   output$diagram <- renderGrViz({
     grViz(
-      get_graph(dm, rankdir = input$rankdir, view_type = input$view_type)
+      get_graph(datamodel, rankdir = input$rankdir, view_type = input$view_type)
     )
     
   })
   output$downloadData <- downloadHandler(
     filename = "export.svg",
     content = function(file) {
-      graph <- dm_create_graph(dm, 
+      graph <- dm_create_graph(datamodel, 
                                graph_attrs = "bgcolor = '#F4F0EF', splines = ortho ",
                                rankdir = input$rankdir, view_type = input$view_type,
                                edge_attrs = "dir = both, arrowtail = crow, arrowhead = odiamond",
@@ -176,7 +143,7 @@ server = function(input, output, session){
   output$downloadDataPng <- downloadHandler(
     filename = "export.png",
     content = function(file) {
-      graph <- dm_create_graph(dm, 
+      graph <- dm_create_graph(datamodel, 
                                graph_attrs = "bgcolor = '#F4F0EF', splines = ortho ",
                                rankdir = input$rankdir, view_type = input$view_type,
                                edge_attrs = "dir = both, arrowtail = crow, arrowhead = odiamond",
